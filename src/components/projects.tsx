820697c8652a775457a1420ca74e80ffe991dd90
João Victor <80591758+VictordamasADS@users.noreply.github.com>
2026-04-23 11:27:47 -0300

    adding my projects into my company

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -4,6 +4,11 @@ import Home2 from "../assets/projects/home-2.jpeg";
 import Panel from "../assets/projects/panel.jpeg";
 import Tool1 from "../assets/projects/tool-1.png";
 import Tool10 from "../assets/projects/tool-10.png";
+import Tool11 from "../assets/projects/tool-11.png";
+import Tool12 from "../assets/projects/tool-12.png";
+import Tool13 from "../assets/projects/tool-13.png";
+import Tool14 from "../assets/projects/tool-14.png";
+import Tool15 from "../assets/projects/tool-15.png";
 import Tool2 from "../assets/projects/tool-2.png";
 import Tool3 from "../assets/projects/tool-3.png";
 import Tool4 from "../assets/projects/tool-4.png";
@@ -29,6 +34,11 @@ export function Projects() {
     Tool8,
     Tool9,
     Tool10,
+    Tool11,
+    Tool12,
+    Tool13,
+    Tool14,
+    Tool15,
   ];
 
   return (
@@ -39,7 +49,7 @@ export function Projects() {
             <img
               key={i}
               src={photo}
-              className="w-80 object-cover shrink-0 shadow-white shadow-2xs rounded-lg"
+              className="w-80 object-cover shrink-0 rounded-lg"
             />
           ))}
         </div>
